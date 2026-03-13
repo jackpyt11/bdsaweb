44ced22add2c96793f0c71a424eb743c2f80c123
Jononi Computer <jononicomputer21@gmail.com>
2026-03-13 23:16:20 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4254826036-be839",
+  "appId": "1:665773092940:web:6cee627f89ed04a30304e9",
+  "apiKey": "AIzaSyDGebnauf-W37HgRDArJxRzZN6FN5NVyDA",
+  "authDomain": "studio-4254826036-be839.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "665773092940"
+};
